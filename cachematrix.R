5755c2fcf5184c2426cb31a585c9408fcca717be
Richard <barajasr89@gmail.com>
2014-06-12 02:44:26 -0700

    Initial commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix parallels makeVector example from README.md.
+## makeCacheMatrix provide structure/functionality for matrix 
+## and it's inverse form.
+## cacheSolve parallels cachemean example from README.md.
+## cacheSolve provides a mechanism to eliminate redundant inverse
+## matrix computation.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special matrix containter in list form.
+## List elements are get/set matrix and get/set_inverse matrix functions.
 makeCacheMatrix <- function(x = matrix()) {
-
+    x_inverse <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        x_inverse <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(inverse) x_inverse <<- inverse
+    get_inverse <- function() x_inverse
+    
+    list(set = set,
+         get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
+    
 }
 
-
-## Write a short comment describing this function
-
+## If inverse matrix x already computed, it is returned.
+## If not computed, it is computed, cached and returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    x_inverse <- x$get_inverse()
+    if(!is.null(x_inverse)) {
+        message("getting cached data")
+        return(x_inverse)
+    }
+    
+    x_inverse <- solve(x$get(), ...)
+    x$set_inverse(x_inverse)
+    x_inverse    
 }
